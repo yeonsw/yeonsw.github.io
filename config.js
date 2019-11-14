4c480cad724d39db6b26bef470b5992c0a363fea
yeonsw <yeon.seonwoo95@gmail.com>
2019-11-14 14:33:43 +0000

    change google sheets id

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const googleApiKey = 'AIzaSyBae-sGk8B9EU9pK5Co1PEhE3zKpvJTnX8';
-export const googleSheetDocId = '16ZUhQoi_TOWNHvZAKrheX6GS3vXwLzTjkeDRKHN1LqY';
+export const googleSheetDocId = '1b4uKT8JHBO37CBpiS_-0lbNhOBcgTDzOCbpsk5-TtYM';
